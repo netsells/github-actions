ef4728c5d4489331a7f393a1a8c75820069181c2
Sam Turrell <sam.turrell@netsells.co.uk>
2024-05-03 17:42:42 +0100

    v1 - Remove conditions

--- reviewdog-vue-tsc/script.sh
+++ reviewdog-vue-tsc/script.sh
@@ -4,23 +4,10 @@ cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ ! -f "$(npm root)"/.bin/tsc ]; then
-  echo "::group::🔄 Running npm install to install tsc ..."
-  npm install
-  echo "::endgroup::"
-fi
-
-if [ ! -f "$(npm root)"/.bin/tsc ]; then
-  echo "❌ Unable to locate or install tsc. Did you provide a workdir which contains a valid package.json?"
-  exit 1
-else
-
-echo ℹ️ tsc version: "$("$(npm root)"/.bin/tsc --version)"
-
 echo "::group::📝 Running vue-tsc with reviewdog 🐶 ..."
 
 # shellcheck disable=SC2086
-npx -p vue-tsc -p typescript vue-tsc --noEmit ${INPUT_TSC_FLAGS} |
+npx --yes -p vue-tsc -p typescript vue-tsc --noEmit ${INPUT_TSC_FLAGS} |
   reviewdog -f=tsc \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER}" \
